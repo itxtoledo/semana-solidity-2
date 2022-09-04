7182106def0c0956e3b3601906fd090e65c6318b
Gustavo <gustavo@obex.pw>
2022-09-04 20:51:23 -0300

    urna funcionando com sons e foto do presida

---
+++ dapp/src/etapas.ts
@@ -0,0 +1,16 @@
+export const etapas = [
+  {
+    titulo: "PRESIDENTE",
+    numeros: 2,
+    legendas: ["Presidente", "Vice-Presidente"],
+    candidatos: [
+      {
+        numero: "22",
+        nome: "BOLSONARO",
+        partido: "Partido Liberal",
+        vice: "MARILENE",
+        fotos: [{ url: "image/president/jair-bolsonaro.webp" }],
+      },
+    ],
+  },
+];
