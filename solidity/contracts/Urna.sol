d9e4296da57589449359bc24c9e56083ad39de5c
Gustavo <gustavo@obex.pw>
2022-09-11 22:36:26 -0300

    added struct

--- solidity/contracts/Urna.sol
+++ solidity/contracts/Urna.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
-struct Candidato {
-    uint8 number;
-    string name;
-}
-
 contract Urna {
+    struct Candidato {
+        string name;
+        uint256 votes;
+    }
+
+    mapping(uint8 => Candidato) candidates;
+
     
 
-    constructor() {
-        
+    createCandidate(string memory name, uint8 number)   external {
+        candidates[number].name = name;
     }
 }
